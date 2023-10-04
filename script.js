3c75a9be66bcd915c8518912777a3c494239a0f4
Marcel <142592388+MarcelAnjos@users.noreply.github.com>
2023-10-04 14:44:06 +0000

    Aula divertida

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.write("Ola, Mundo");
-console.log("Imprimindo no console!");
-window.alert('Texto no modal do navegador!');
-console.log();
+//
+//
+//
+
+//cálculo da média
+let nota1;
+let nota2;
+let nota3;
+
+nota1 = parseInt(prompt('Insira a ntoa 1'));
+nota2 = parseInt(prompt('Insira a nota 2'));
+nota3 = parseInt(prompt('Insira a nota 3'));
+media = (nota1 + nota2 + nota3) / 3 ;
+
+console.log('A nota 1 é: ' + nota1);
+console.log('A nota 2 é: ' + nota2);
+console.log('A nota 3 é: ' + nota3);
+console.log('A nota final do aluno:' + media); 
+
